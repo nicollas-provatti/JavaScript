7bb33d57cf88b89b9ce962eb2ad82d55d672157b
nicollas-provatti <npg@ic.ufal.br>
2025-05-11 14:08:45 -0300

    Delete Aula 2 directory

--- Aula 2/6 - template string.js
+++
@@ -1,8 +0,0 @@
-let nome = "Nícollas";
-let idade = 23
-
-let mensagem1 = "Olá, " + nome + "! Você tem " + idade + " anos.";
-let mensagem2 = `Olá, ${nome}! Você tem ${idade} anos.`;
-
-console.log(mensagem1);
-console.log(mensagem2);
